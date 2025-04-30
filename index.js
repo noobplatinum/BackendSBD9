b67055fdd52b520b161d0168fbe22f41f000e64e
J David P <jesayadavidgnp@gmail.com>
2025-04-30 14:21:40 +0700

    FAK

--- index.js
+++ index.js
@@ -12,8 +12,7 @@ const corsOptions = {
         }
     
         const allowedOrigins = [
-            "https://frontend-sbd-9.vercel.app",
-            "frontend-sbd-9-fy2nrbziy-noobplatinums-projects.vercel.app",
+            "https://cs9-jesayadavidgnp-fe.vercel.app",
             "https://os.netlabdte.com",
             "http://localhost:3000",
             "http://localhost:8080",
